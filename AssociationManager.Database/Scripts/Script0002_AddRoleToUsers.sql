63fb44b0e3b362c28a95f943c9c76f01adc0b0dc
BaskarKCareer <138487518+BaskarKCareer@users.noreply.github.com>
2026-04-12 22:36:19 -0400

    cleanup: remove obsolete migration scripts

--- AssociationManager.Database/Scripts/Script0002_AddRoleToUsers.sql
+++
@@ -1,5 +0,0 @@
-IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID('Users') AND name = 'Role')
-BEGIN
-    ALTER TABLE Users ADD Role NVARCHAR(50) NOT NULL DEFAULT 'User';
-END
-GO
