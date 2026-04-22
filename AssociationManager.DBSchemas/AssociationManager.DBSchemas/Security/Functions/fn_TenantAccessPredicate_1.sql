e24b28a7a96317c86c18c9afeb19988a30efb8f2
BaskarKCareer <138487518+BaskarKCareer@users.noreply.github.com>
2026-04-21 23:21:02 -0400

    Cleanup redundant _1.sql files and synchronize .sqlproj file

--- AssociationManager.DBSchemas/AssociationManager.DBSchemas/Security/Functions/fn_TenantAccessPredicate_1.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE FUNCTION Security.fn_TenantAccessPredicate(@TenantId INT)
-    RETURNS TABLE
-    WITH SCHEMABINDING
-AS
-    RETURN SELECT 1 AS fn_access_result
-    WHERE 
-        (CAST(SESSION_CONTEXT(N'TenantId') AS INT) = @TenantId)
-        OR (CAST(SESSION_CONTEXT(N'IsAdmin') AS INT) = 1);
